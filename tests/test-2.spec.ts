4356f3788141fd0b3eccafc92b6740ac1390103b
Rohit Singh <rohit.0008singh@gmail.com>
2025-07-25 01:10:48 +0530

    addind code to ado

--- tests/test-2.spec.ts
+++ tests/test-2.spec.ts
@@ -4,4 +4,5 @@ test('test', async ({ page }) => {
   await page.goto('https://rahulshettyacademy.com/AutomationPractice/');
   await page.getByRole('button', { name: 'Alert' }).click();
   await page.getByRole('button', { name: 'Confirm' }).click();
+  console.log("hi my name is rohit");
 });
