0e1e9b85d6c342a7a0342791b774df51ca592a75
Rohit Singh <rohit.0008singh@gmail.com>
2025-07-30 01:10:44 +0530

    updated test-2 class

--- playwright.config.ts
+++ playwright.config.ts
@@ -22,7 +22,7 @@ export default defineConfig({
   /* Retry on CI only */
   retries: process.env.CI ? 2 : 0,
   /* Opt out of parallel tests on CI. */
-  workers: process.env.CI ? 1 : undefined,
+  workers: process.env.CI ? 4 : undefined,
   /* Reporter to use. See https://playwright.dev/docs/test-reporters */
   reporter: 'html',
   //['html', { open: 'never' }]],
@@ -32,7 +32,7 @@ export default defineConfig({
     colorScheme: 'dark',
       // Capture screenshot after each test failure.
       screenshot: 'on',
-      headless: true,
+      headless: false,
       trace: 'retain-on-failure',
     
 
--- tests/test-2.spec.ts
+++ tests/test-2.spec.ts
@@ -1,8 +1,43 @@
-import { test, expect } from '@playwright/test';
-
+import { test, expect,Browser,Page,chromium } from '@playwright/test';
+import { Context } from 'vm';
+test.describe.configure({mode:'parallel'})
 test('test', async ({ page }) => {
   await page.goto('https://rahulshettyacademy.com/AutomationPractice/');
   await page.getByRole('button', { name: 'Alert' }).click();
   await page.getByRole('button', { name: 'Confirm' }).click();
   console.log("hi my name is rohit");
+  page.close();
 });
+
+
+test('test paraller or in series',async()=>{
+
+const browser:Browser= await chromium.launch();
+const context:Context= await browser.newContext();
+const page:Page= await context.newPage();
+
+await page.goto('https://rahulshettyacademy.com/AutomationPractice/');
+
+page.close();
+
+})
+test('validiting javascript alerts',async()=>{
+
+  const browser1:Browser= await chromium.launch();
+  const context:Context= await browser1.newContext();
+  const page1:Page= await context.newPage();
+
+  await page1.goto("https://rahulshettyacademy.com/AutomationPractice/");
+  let alertMessage = "";
+
+  page1.on('dialog', async dialog => {
+    alertMessage = dialog.message();  // capture alert text
+    await dialog.accept();            // or dialog.dismiss()
+  });
+  
+  await page1.click('#alertbtn');  // trigger the alert
+  
+  console.log("Alert message:", alertMessage);
+  page1.close();
+
+})
